691448eb7b6358656c37b52b2bb845c95e9d45bc
Hashim K Jacobs <granulargodd@Hashims-MacBook-Pro.local>
2019-11-05 00:56:14 -0800

    initial commit

---
+++ src/components/Scene.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import * as THREE from "three";
+
+export default class Scene extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return null;
+  }
+}
