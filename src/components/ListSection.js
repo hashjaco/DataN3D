07a4f964ba3a004f299b6a67f3d5c54a73be1a58
Mvrs <johnsonmarlon18@gmail.com>
2019-11-20 22:59:09 -0800

    feat(output-data-leftcontainer): created markdown/references for project ref

--- src/components/ListSection.js
+++ src/components/ListSection.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 
 const ListSection = ({ name, dataPointX, dataPointY, dataPointZ }) => {
+  // TODO styling for each DataCard is the next thing that needs to be done
+
   return (
     <>
       <li>Name: {name}</li>
