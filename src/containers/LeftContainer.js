da313db9d6eeccf3f8c03c48892aba1d74f3c8b6
Hashim K Jacobs <granulargodd@Hashims-MacBook-Pro.local>
2019-11-26 17:50:25 -0800

    hotfix for homepage layout

--- src/containers/LeftContainer.js
+++ src/containers/LeftContainer.js
@@ -1,33 +1,36 @@
-import React from 'react'
-import ListSection from '../components/ListSection'
+import React from "react";
+import ListSection from "../components/ListSection";
 
-const data = require('../data/spiral') || []
+const data = require("../data/spiral") || [];
 
 const LeftContainer = () => {
-  return data.map(context => (
+  return (
     <div style={styles.leftContainer}>
-      <>
-        <ListSection
-          key={context.name}
-          name={context.name}
-          dataPointX={context.x}
-          dataPointY={context.y}
-          dataPointZ={context.z}
-        />
-      </>
+      {data.map(context => (
+        <>
+          <ListSection
+            key={context.name}
+            name={context.name}
+            dataPointX={context.x}
+            dataPointY={context.y}
+            dataPointZ={context.z}
+          />
+        </>
+      ))}
+      }
     </div>
-  ))
-}
+  );
+};
 
 const styles = {
   leftContainer: {
-    display: 'inline-block',
-    height: '100%',
-    borderRightStyle: 'solid',
-    borderColor: '#343a40',
+    display: "inline-block",
+    height: "100%",
+    borderRightStyle: "solid",
+    borderColor: "#343a40",
     borderWidth: 0.5,
-    width: '20%'
+    width: "20%"
   }
-}
+};
 
-export default LeftContainer
+export default LeftContainer;
