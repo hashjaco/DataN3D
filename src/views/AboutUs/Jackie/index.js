a14061bdf2552f8cc59b9acdb959b9256aa5ccc3
jshan430 <jshan@mail.ccsf.edu>
2019-12-06 00:55:12 -0800

    J Shan about me page has been updated

--- src/views/AboutUs/Jackie/index.js
+++ src/views/AboutUs/Jackie/index.js
@@ -5,14 +5,22 @@ import "./index.css"
 export default class Jackie extends Component {
   constructor(props) {
     super(props);
-
+    
   }
 
   render() {
     return (
-      <div>
-        <Header title="Jackie" />
+      <div className="bg">
+        <div className="main-text">
+          <h5>Hello I'm</h5>
+          <h1>Jackie Shan</h1>
+          <h5>Student</h5>
+          <h6>Computer Science Major at SFSU</h6>
+          <p>My inspirations are to become a web and mobile app developer.</p>
+        </div>
       </div>
+      
     );
   }
 }
+/*<Header title="Jackie" />*/
